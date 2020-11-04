75cdde57eb1e77c89e3afcf3ff49cf78dcafd2e4
Ian Muñoz Nuñez <ian.munoz4644@alumnos.udg.mx>
2020-11-04 11:47:09 -0600

    Se agregaron las funciones abrir y guardar para el archivo .json

--- Particula/mainclass.py
+++ Particula/mainclass.py
@@ -1,16 +1,36 @@
+import json
 from Particula.particula import Particula
 
 class MainClass:
     def __init__(self):
         self.__particulas = []
+    
     def agregar_inicio(self, particula: Particula):
         self.__particulas.insert(0, particula)
+    
     def agregar_final(self, particula: Particula):
         self.__particulas.append(particula)
-    def imprimir(self):
-        for particula in self.__particulas:
-            print(particula)
+    
     def __str__(self):
         return "".join(
             str(particula) for particula in self.__particulas
         )
+    
+    def abrir(self, ubicacion):
+        try:
+            with open(ubicacion, 'r') as archivo:
+                lista = json.load(archivo)
+                self.__particulas = [Particula(**particula) for particula in lista]
+            return 1
+        except:
+            return 0
+
+    def guardar(self, ubicacion):
+        try:
+            with open(ubicacion, "w") as archivo:
+                lista = [particula.to_dict() for particula in self.__particulas]
+                json.dump(lista, archivo, indent=5)
+            return 1
+        except:
+            return 0
+
