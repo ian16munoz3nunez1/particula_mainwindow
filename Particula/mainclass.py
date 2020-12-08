d3e0f112d894aafc3b6cf3f1b811cd6e50cd367d
Ian Muñoz Nuñez <ian.munoz4644@alumnos.udg.mx>
2020-12-08 15:26:25 -0600

    Archivo con las nuevas funciones 'busqueda_amplitud()' y 'busqueda_profundidad()'

--- Particula/mainclass.py
+++ Particula/mainclass.py
@@ -1,4 +1,6 @@
 import json
+from pprint import pformat
+from collections import deque
 from Particula.particula import Particula
 
 class MainClass:
@@ -49,3 +51,76 @@ class MainClass:
         except:
             return 0
 
+    def crear_grafo(self, grafo):
+        for particula in self.__particulas:
+            origen_x = particula.origen_x
+            origen_y = particula.origen_y
+            destino_x = particula.destino_x
+            destino_y = particula.destino_y
+            distancia = particula.distancia
+
+            origen = (origen_x, origen_y)
+            destino = (destino_x, destino_y)
+            arista_o = ((destino_x, destino_y), distancia)
+            arista_d = ((origen_x, origen_y), distancia)
+
+            if origen in grafo:
+                grafo[origen].append(arista_o)
+            else:
+                grafo[origen] = [arista_o]
+            if destino in grafo:
+                grafo[destino].append(arista_d)
+            else:
+                grafo[destino] = [arista_d]
+        str = pformat(grafo, width=50, indent=1)
+        return str
+
+    def busqueda_amplitud(self, grafo, origen_x=0, origen_y=0):
+        self.crear_grafo(grafo)
+        origen = (origen_x, origen_y)
+
+        if origen in grafo:
+            cola = deque()
+            visitados = []
+            recorrido = []
+
+            cola.appendleft(origen)
+            visitados.append(origen)
+
+            while len(cola) > 0:
+                vertice = cola[-1]
+                recorrido.append(vertice)
+                cola.pop()
+                adyacente = grafo[vertice]
+                for i in adyacente:
+                    if not i[0] in visitados:
+                        visitados.append(i[0])
+                        cola.appendleft(i[0])
+            return recorrido
+        else:
+            return 0
+
+    def busqueda_profundidad(self, grafo, origen_x=0, origen_y=0):
+        self.crear_grafo(grafo)
+        origen = (origen_x, origen_y)
+
+        if origen in grafo:
+            pila = []
+            visitados = []
+            recorrido = []
+
+            pila.append(origen)
+            visitados.append(origen)
+
+            while len(pila) > 0:
+                vertice = pila[-1]
+                recorrido.append(vertice)
+                pila.pop()
+                adyacente = grafo[vertice]
+                for i in adyacente:
+                    if not i[0] in visitados:
+                        visitados.append(i[0])
+                        pila.append(i[0])
+            return recorrido
+        else:
+            return 0
